f9aa0a450c28d3b68a4bad6f607ba472ff670d0c
eagleaie <nomail@nomail.com>
2013-09-29 21:56:21 +0200

    start project

---
+++ src/main/java/fr/catsoft/prolog/Exemple.java
@@ -0,0 +1,15 @@
+package fr.catsoft.prolog;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: remy
+ * Date: 29/09/13
+ * Time: 21:52
+ * To change this template use File | Settings | File Templates.
+ */
+public class Exemple {
+
+    public static final void main(String[] args) {
+
+    }
+}
